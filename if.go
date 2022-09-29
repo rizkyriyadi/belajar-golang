5f56722de93fc0dc71261d1c8f73e18f86acbdaf
Rizky Riyadi <rizkyriady16@gmail.com>
2022-09-29 20:21:39 +0700

    Percabangan pada Golang

---
+++ if.go
@@ -0,0 +1,18 @@
+/* Percabangan
+Sama seperti bahasa lain if elif else
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	umur := 19
+
+	if umur <= 18 {
+		fmt.Println("Belum Cukup Umur")
+	} else {
+		fmt.Println("Sudah Cukup Umur")
+	}
+}
